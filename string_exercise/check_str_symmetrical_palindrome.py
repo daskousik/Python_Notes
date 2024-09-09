aa836e5c2809b4c652cdff30298fd8055000546e
daskousik <daskousik799@gmail.com>
2024-09-09 15:37:14 +0530

    Add symmentrical string  check

---
+++ string_exercise/check_str_symmetrical_palindrome.py
@@ -0,0 +1,17 @@
+# Python program to check whether the string is Symmetrical or Palindrome..
+# Given a string. the task is to check if the string is symmetrical and palindrome or not.
+# A string is said to be symmetrical if both the halves of the string are the same
+# and a string is said to be a palindrome string if one half of the string is the reverse of the other half or if a string appears the same
+# when read forward or backward.
+
+# Input: khokho
+# Output:
+# The entered string is symmetrical
+# The entered string is not palindrome
+#
+# Input:amaama
+# Output:
+# The entered string is symmetrical
+# The entered string is palindrome
+
+input_str = 'khokho'
